03ed9a0bcc00746d8dfe4f17e04eddc1f32239ee
Kadu <kadu23977@gmail.com>
2023-10-25 13:40:45 -0300

    add ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-project';
+  name = 'my first angular project';
 }
